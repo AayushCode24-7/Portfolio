48856dc1ca86d3ef1243b689e27aa29acceb6e8d
Aayush <aayushjindal65@gamil.com>
2026-06-14 17:51:43 +0530

    added gsap to pages

---
+++ script.js
@@ -0,0 +1,30 @@
+gsap.to("#page1",{
+    scrollTriger:{
+        trigger: "#page1",
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        pin:true,
+        scroller: '#main'
+    }
+})
+gsap.to("#page2",{
+    scrollTriger:{
+        trigger: "#page1",
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        pin:true,
+        scroller: '#main'
+    }
+})
+gsap.to("#page3",{
+    scrollTriger:{
+        trigger: "#page1",
+        start: 'top top',
+        end: 'bottom top',
+        markers: true,
+        pin:true,
+        scroller: '#main'
+    }
+})
